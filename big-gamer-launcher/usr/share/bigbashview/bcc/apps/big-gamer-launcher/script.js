3800276b7cc8c51f753246dc871062c3a85c5e5e
Ruscher <rruscher@gmail.com>
2022-07-21 10:25:47 -0300

    New options

--- big-gamer-launcher/usr/share/bigbashview/bcc/apps/big-gamer-launcher/script.js
+++ big-gamer-launcher/usr/share/bigbashview/bcc/apps/big-gamer-launcher/script.js
@@ -5,16 +5,6 @@ toggleButton.addEventListener("click", () => {
   _run('/usr/share/bigbashview/bcc/shell/setbgcolor.sh "' + document.body.classList.contains('light-mode') + '"');
 });
 
-
-
-
-
-
-
-
-
-
-
 const navIcons = document.querySelectorAll("nav ul li");
 
 const hamBurger = document.querySelector("#hamburger");
@@ -24,15 +14,9 @@ const sidebar = document.querySelector("aside");
 const searchBar = document.querySelector(".search");
 
 const setActive = (e) => {
-	navIcons.forEach((navIcon) => navIcon.classList.remove("active"));
-	e.currentTarget.classList.add("active");
-};
-
-$(document).on("click", "#point-container", function () {
-  var show = $(this).data("show");
-  alert(show);
-  $(show).removeClass("hide").siblings().addClass("hide");
-});
+ 	navIcons.forEach((navIcon) => navIcon.classList.remove("active"));
+ 	e.currentTarget.classList.add("active");
+ };
 
 const expand = () => {
 	searchBar.querySelector("input").focus();
@@ -50,7 +34,7 @@ const closeSidebar = (e) => {
 };
 
 navIcons.forEach((navIcon) => {
-	navIcon.addEventListener("click", setActive);
+	navIcon.addEventListener("click", setActive);		
 });
 
 hamBurger.addEventListener("click", showSidebar);
@@ -58,3 +42,34 @@ hamBurger.addEventListener("click", showSidebar);
 searchBar.addEventListener("click", expand);
 
 document.addEventListener("click", closeSidebar);
+
+
+$(document).on("click", "#point-container", function () {
+  var show = $(this).data("show");
+  //alert(show);
+  $(show).removeClass("hide").siblings().addClass("hide");
+});
+
+
+
+
+/* Example code for updating a chart with JS */
+
+function loop(t) {
+  requestAnimationFrame(loop);
+  updateCharts(Math.floor(t / 16) % 100);
+}
+
+function updateCharts(value) {
+  charts.forEach(chart => setChartValue(chart, value));
+}
+
+function setChartValue(chart, value) {
+  chart.style.setProperty('--percent', value);
+}
+
+let charts = document.querySelectorAll('.js');
+
+loop();
+
+
